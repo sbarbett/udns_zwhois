a1bffb4ce6975181fc83531cd8d4d321aacd2d63
sbarbett <shane@barbetta.me>
2023-09-22 01:11:10 -0400

    Added a progress bar, code to automatically refresh a token when using credentials and logic to only perform whois lookups on base domains

--- README.md
+++ README.md
@@ -1,6 +1,6 @@
 # udns_zwhois
 
-`udns_zwhois` is a Python-based CLI tool designed to fetch Zone properties from UltraDNS and output it with some additional information gathered from WHOIS.
+`udns_zwhois` is a Python-based CLI tool that interfaces with Vercara's UltraDNS API to fetch zone properties. It also supplements this data with additional information sourced from WHOIS lookups.
 
 ## Directory Structure
 
@@ -17,41 +17,66 @@ udns_zwhois
 ## Requirements
 
 - Python 3.x
-- `requests`
-- `python-whois`
+- The following Python libraries:
+  - `requests`
+  - `python-whois`
+  - `tqdm`
 
-You can install the required Python packages using the following command:
+### Installation
 
-```
+To install the required Python packages, navigate to the root directory of the project and execute:
+
+```bash
 pip install -r requirements.txt
 ```
 
 ## Usage
 
-You can run the `zwhois.py` script from the command line using the following syntax:
+Run the `zwhois.py` script from the command line:
 
-```
+### Using credentials:
+
+```bash
 python3 src/zwhois.py --username YOUR_USERNAME --password YOUR_PASSWORD
 ```
 
-Or by using a bearer token:
+### Using a bearer token:
 
-```
+```bash
 python3 src/zwhois.py --token YOUR_BEARER_TOKEN
 ```
 
-Optionally, you can write the output to a file in JSON or CSV format:
+_Note: If you choose to supply a bearer token directly to the script, it will not be able to automatically refresh when it expires. For long-running processes, I recommend using credentials._
 
-```
-python3 src/zwhois.py --token YOUR_BEARER_TOKEN --output-file output.json
+### Output Options:
+
+You can specify the desired output format (either JSON or CSV) and write the results to a file:
+
+```bash
+python3 src/zwhois.py {AUTH_DETAILS} --output-file output.json --format json
 ```
 
-Or
+Or for CSV format:
 
+```bash
+python3 src/zwhois.py {AUTH_DETAILS} --output-file output.csv --format csv
 ```
-python3 src/zwhois.py --token YOUR_BEARER_TOKEN --output-file output.csv --format csv
-```
+
+## Examples
+
+- Fetching data using credentials and saving to a CSV:
+
+  ```bash
+  python3 src/zwhois.py --username admin --password secret123 --output-file data.csv --format csv
+  ```
+
+- Fetching data using a token and displaying on the terminal:
+
+  ```bash
+  python3 src/zwhois.py --token ABCDEFGHIJKLMNOPQRSTUVWXYZ
+  ```
 
 ## License
 
 This project is licensed under the terms of the MIT license. For more details, see [LICENSE.md](./LICENSE.md).
+```
--- src/zwhois.py
+++ src/zwhois.py
@@ -4,6 +4,8 @@ from pprint import pprint
 import whois
 import json
 import csv
+import time
+from tqdm import tqdm
 
 def get_ultradns_access_token(username, password):
     token_url = "https://api.ultradns.com/authorization/token"
@@ -15,33 +17,91 @@ def get_ultradns_access_token(username, password):
     response = requests.post(token_url, data=data)
     response.raise_for_status()
     token_data = response.json()
-    return token_data.get('accessToken')
+    return token_data.get('accessToken'), token_data.get('refreshToken'), token_data.get('expiresIn')
+
+def refresh_ultradns_access_token(refresh_token):
+    token_url = "https://api.ultradns.com/authorization/token"
+    data = {
+        'grant_type': 'refresh_token',
+        'refresh_token': refresh_token
+    }
+    response = requests.post(token_url, data=data)
+    response.raise_for_status()
+    token_data = response.json()
+    return token_data.get('accessToken'), token_data.get('expiresIn')
+
+def api_request(url, token, method=requests.get, headers=None, **kwargs):
+    """Centralized function for making API requests. This handles token refresh and 401 errors."""
+    if not headers:
+        headers = {}
+    headers['Authorization'] = f'Bearer {token}'
+    
+    response = method(url, headers=headers, **kwargs)
+    if response.status_code == 401 and refresh_token:  # Token expired, try refreshing
+        token, _ = refresh_ultradns_access_token(refresh_token)
+        headers['Authorization'] = f'Bearer {token}'
+        response = method(url, headers=headers, **kwargs)
+    elif response.status_code == 401:
+        raise Exception("Your bearer token has expired. The script took longer than the token's validity. Please use credentials instead.")
+    
+    response.raise_for_status()
+    
+    return response.json()
 
 def get_zones(token):
     zones_url = "https://api.ultradns.com/v2/zones"
-    headers = {'Authorization': f'Bearer {token}'}
-    response = requests.get(zones_url, headers=headers)
-    response.raise_for_status()
-    return response.json().get('zones', [])
+    all_zones = []
+    params = {'limit': 1000}
+    while True:
+        data = api_request(zones_url, token, params=params)
+        time.sleep(0.5)
+        all_zones.extend(data.get('zones', []))
+
+        # Pagination
+        next_cursor = data.get('cursorInfo', {}).get('next')
+        if not next_cursor:
+            break
+        params['cursor'] = next_cursor
+
+    return all_zones
 
 def get_soa_record(zone_name, token):
     soa_url = f"https://api.ultradns.com/v2/zones/{zone_name}/rrsets/SOA/"
-    headers = {'Authorization': f'Bearer {token}'}
-    response = requests.get(soa_url, headers=headers)
-    response.raise_for_status()
-    rdata = response.json().get('rrSets', [{}])[0].get('rdata', [''])[0]
+    data = api_request(soa_url, token)
+    time.sleep(0.5)
+    rdata = data.get('rrSets', [{}])[0].get('rdata', [''])[0]
     return rdata.split(' ')[1].replace('\\.', '.')
 
 def get_aliased_domains(token):
-    alias_url = "https://api.ultradns.com/v2/zones/?q=zone_type:ALIAS&limit=1000"
-    headers = {'Authorization': f'Bearer {token}'}
-    response = requests.get(alias_url, headers=headers)
-    response.raise_for_status()
-    return {zone['originalZoneName']: zone['properties']['name'] for zone in response.json().get('zones', [])}
+    base_url = "https://api.ultradns.com/v2/zones/"
+    params = {'q': 'zone_type:ALIAS', 'limit': 1000}
+    all_aliased_domains = {}
+
+    while True:
+        data = api_request(base_url, token, params=params)
+        time.sleep(0.5)
+        all_aliased_domains.update(
+            {zone['originalZoneName']: zone['properties']['name'] for zone in data.get('zones', [])}
+        )
+
+        # Pagination
+        next_cursor = data.get('cursorInfo', {}).get('next')
+        if not next_cursor:
+            break
+        params['cursor'] = next_cursor
+
+    return all_aliased_domains
+
+def get_base_domain(domain):
+    parts = domain.split('.')
+    if len(parts) > 2:
+        return ".".join(parts[-2:])
+    return domain
 
 def get_whois_info(domain):
     try:
-        domain_info = whois.whois(domain)
+        base_domain = get_base_domain(domain)
+        domain_info = whois.whois(base_domain)
         expiration_date = domain_info.expiration_date
         if isinstance(expiration_date, list):
             expiration_date = expiration_date[0]
@@ -76,8 +136,11 @@ if __name__ == "__main__":
 
     if args.token:
         token = args.token
+        refresh_token = None  # No refresh token when provided directly
+        token_expiry_time = None  # Indeterminate expiry time
     elif args.username and args.password:
-        token = get_ultradns_access_token(args.username, args.password)
+        token, refresh_token, expires_in = get_ultradns_access_token(args.username, args.password)
+        token_expiry_time = time.time() + expires_in  # Record the absolute expiry time
     else:
         parser.error("You must provide either a username and password or a bearer token.")
 
@@ -85,9 +148,19 @@ if __name__ == "__main__":
     aliased_domains_map = get_aliased_domains(token)
 
     report = []
-    
-    for zone in zones:
-        zone_name = zone['properties']['name'].rstrip('.')
+
+# tqdm will show progress for processing zones
+for zone in tqdm(zones, desc="Processing Zones", ncols=100):
+
+    # Check if token is about to expire
+    if token_expiry_time and time.time() > token_expiry_time - 10:  # 10 seconds buffer
+        if not refresh_token:
+            raise Exception("Operation too long. Access token expired and no refresh token available. Use credentials instead.")
+        token, expires_in = refresh_ultradns_access_token(refresh_token)
+        token_expiry_time = time.time() + expires_in
+
+    zone_name = zone['properties']['name'].rstrip('.')
+    try:
         zone_contact_email = get_soa_record(zone_name, token)
         alias = aliased_domains_map.get(zone_name, None)
         registrar, expiration_date = get_whois_info(zone_name)
@@ -102,6 +175,8 @@ if __name__ == "__main__":
             "Zone Type": zone['properties']['type'],
             "Resource Record Count": zone['properties']['resourceRecordCount']
         })
+    except Exception as e:
+        print(f"Error processing zone {zone_name}: {e}")
     
     if args.output_file:
         if args.format:
